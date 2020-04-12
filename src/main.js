379cfb588c8c9e89e65612a26e2308bf1daae724
lifenglei <1125911451@qq.com>
2020-04-12 22:16:05 +0800

    基础模板

--- src/main.js
+++ src/main.js
@@ -4,8 +4,8 @@ import router from './router';
 import ElementUI from 'element-ui';
 import VueI18n from 'vue-i18n';
 import { messages } from './components/common/i18n';
-import 'element-ui/lib/theme-chalk/index.css'; // 默认主题
-// import './assets/css/theme-green/index.css'; // 浅绿色主题
+// import 'element-ui/lib/theme-chalk/index.css'; // 默认主题
+import './assets/css/theme-green/index.css'; // 浅绿色主题
 import './assets/css/icon.css';
 import './components/common/directives';
 import 'babel-polyfill';
